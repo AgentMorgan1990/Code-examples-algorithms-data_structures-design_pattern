2e1862145cac5d88810d6d55bf36ea3410cb35a2
Sergei Afonin <s.afonin@bankffin.ru>
2022-07-07 22:51:43 +0300

    Add selection sort algorithm

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int[] array = {45, 89, 78, 90, 500, 456, 34, 23, 1, 67, 4, 34, 56, 7};
+        System.out.println(Arrays.toString(array));
+        int[] sortedArray = SelectionSort.selectionSort(array);
+        System.out.println(Arrays.toString(sortedArray));
+
+        System.out.println("Hello World!");
     }
 }
